db93f4c81a2c0e1a19bfd98db974fee5c5558fa4
Darkgel <1627116986@qq.com>
2019-01-02 16:21:30 +0800

    配置swagger

--- app/Api/Controllers/V1/Blog/ArticleController.php
+++ app/Api/Controllers/V1/Blog/ArticleController.php
@@ -16,6 +16,46 @@ use Enum\ErrorCode;
 
 class ArticleController extends V1Controller
 {
+    /**
+     * 文章详情
+     * @author darkgel
+     * @date 2019/1/2
+     */
+    /**
+     * @SWG\Get(
+     *     path="/blog/article/{id}",
+     *     summary="获取文章详情",
+     *     tags={"Blog/Article"},
+     *     description="通过文章id获取相应的文章详情",
+     *     operationId="V1.Blog.Article.detail",
+     *     produces={"application/json"},
+     *     @SWG\Parameter(name="id",in="path",description="文章id",type="integer",required=true),
+     *     @SWG\Response(
+     *          response=200,
+     *          description="success",
+     *          @SWG\Schema(
+     *              type="json",
+     *              @SWG\Property(property="code",type="integer",description="业务逻辑状态码",),
+     *              @SWG\Property(property="msg",type="string",description="业务逻辑处理结果信息",),
+     *              @SWG\Property(
+     *                  property="content",
+     *                  @SWG\Property(
+     *                      property="data", description="文章详情",
+     *                      @SWG\Property(property="id",type="integer",description="文章id",),
+     *                      @SWG\Property(property="title",type="string",description="文章标题",),
+     *                      @SWG\Property(property="author",type="string",description="文章作者",),
+     *                      @SWG\Property(property="updatedAt",type="integer",description="更新时间",),
+     *                      @SWG\Property(property="createdAt",type="integer",description="创建时间",),
+     *                      @SWG\Property(property="summary",type="string",description="文章摘要",),
+     *                      @SWG\Property(property="contentHtml",type="string",description="文章内容（html格式）",),
+     *                      @SWG\Property(property="tags",type="string",description="标签",),
+     *                      @SWG\Property(property="status",type="integer", description="状态",),
+     *                 ),
+     *              ),
+     *          ),
+     *     )
+     *  )
+     */
     public function detail(ArticleRepository $articleRepository, $id){
         try{
             $cacheKey = __METHOD__."_".$id;
--- app/Api/Controllers/V1/V1Controller.php
+++ app/Api/Controllers/V1/V1Controller.php
@@ -14,5 +14,26 @@ use App\Http\Controllers\ApiController;
 
 class V1Controller extends ApiController
 {
-
+    /**
+     * @SWG\Swagger(
+     *     schemes={"http","https"},
+     *     host=L5_SWAGGER_CONST_HOST,
+     *     @SWG\SecurityScheme(
+     *         securityDefinition="client",
+     *         type="apiKey",
+     *         name="Authorization",
+     *         in="header",
+     *     ),
+     *     security={{"client": {}},},
+     *     @SWG\Info(
+     *         version="1.0",
+     *         title=" Team Blog API接口文档",
+     *         description="Team Blog API接口文档",
+     *         @SWG\Contact(
+     *             name="darkgel",
+     *             email="1627116986@qq.com"
+     *         )
+     *     ),
+     * )
+     */
 }
